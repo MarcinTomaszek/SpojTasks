237dc6c6a129b13fc4227c08f286ef05a78a36aa
BlueJack14 <bluejack1414@gmail.com>
2025-01-13 22:06:12 +0100

    Test and Mohib pizza

---
+++ MohibPizza/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text;
+
+int len = int.Parse(Console.ReadLine());
+var output = new StringBuilder();
+for (int i = 0; i < len; i++)
+{
+    int n = int.Parse(Console.ReadLine());
+    long result = (long)n * (n + 1) / 2 + 1;
+    output.AppendLine(result.ToString());
+}
+Console.Write(output);
--- TEST_Life_the_Universe_and_Everything/Program.cs
+++ TEST_Life_the_Universe_and_Everything/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string line;
+        string input = "";
+        while ( !string.IsNullOrEmpty(line=Console.ReadLine()))
+        {
+            input += line+"/n";
+        }
+        var arr = input.Split("/n");
+        
+        foreach (var s in arr)
+        {
+            if (s == "42")
+                break;
+            Console.WriteLine(s);
+        }
+
     }
 }
