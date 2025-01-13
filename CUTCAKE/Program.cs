2733ebf6022d97fa9764a4e42adffde7c71e74ba
BlueJack14 <bluejack1414@gmail.com>
2025-01-13 22:22:06 +0100

    Cut Cake Solve

---
+++ CUTCAKE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+int len = int.Parse(Console.ReadLine());
+var output = new StringBuilder();
+for (int i = 0; i < len; i++)
+{
+    long n = long.Parse(Console.ReadLine());
+    if (n == 1)
+    {
+        output.AppendLine("0");
+        continue;
+    }
+    long result = (long)Math.Ceiling((-1 + Math.Sqrt(8 * (n - 1))) / 2);
+    output.AppendLine(result.ToString());
+}
+Console.Write(output);
